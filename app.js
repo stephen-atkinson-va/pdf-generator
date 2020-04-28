0e1d40cf630814175ba56ad6519ec426356135a6
Stephen Atkinson <stephen.atkinson@ventureaxis.com>
2020-04-28 11:19:35 +0100

    Added html-pdf library and endpoint for generating pdf

--- app.js
+++ app.js
@@ -2,9 +2,32 @@ const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
 
+const fs = require('fs');
+const pdf = require('html-pdf');
 
 app.get('/', (req, res) => res.send('Hello World !!'));
 
 app.get('/toast', (req, res) => res.send('Hello toast !!'));
+
+app.get('/pdf', (req, res) => {
+
+    var data = fs.readFileSync('pdf.html', 'utf8');
+
+    pdf.create(data).toBuffer(function(err, buffer){
+
+        let x = new Uint8Array(buffer);
+
+        var arr = []; 
+        for(var p in Object.getOwnPropertyNames(x)) {
+            arr[p] = x[p];
+        }
+
+        res.setHeader('Content-disposition', 'inline; filename=' + "invoice.pdf");
+        res.setHeader('Content-type', 'application/pdf');
+
+        res.send(buffer);
+      });
+
+});
 
 app.listen(port, () => console.log('Server is running on port ' + port));
