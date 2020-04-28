7837e583b7d3888cb73e119d8dc27b219853eebc
Stephen Atkinson <stephen.atkinson@ventureaxis.com>
2020-04-28 11:12:41 +0100

    Added /toast endpoint

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
+
+
 app.get('/', (req, res) => res.send('Hello World !!'));
+
+app.get('/toast', (req, res) => res.send('Hello toast !!'));
+
 app.listen(port, () => console.log('Server is running on port ' + port));
